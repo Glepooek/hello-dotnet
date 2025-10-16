e5cb0a72adc9c0cf4d32cbe408669f041af6d854
anyu <glepooek@outlook.com>
2025-10-16 23:04:55 +0800

    通过Microsoft.Extensions.Configuration.Json加载appsettings.json

---
+++ ReadAppsettingsDemo/ConfigurationJsonDemo/AppSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationJsonDemo
+{
+    public class AppSettings
+    {
+        public LoggingSettings Logging { get; set; }
+        public ApplicationSettings Application { get; set; }
+    }
+    public class LoggingSettings
+    {
+        public LogLevel LogLevel { get; set; }
+    }
+    public class LogLevel
+    {
+        public string Default { get; set; }
+    }
+    public class ApplicationSettings
+    {
+        public string AppName { get; set; }
+        public string Version { get; set; }
+    }
+}
---
+++ ReadAppsettingsDemo/ConfigurationJsonDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConfigurationJsonDemo;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true); ;
+
+IConfigurationRoot config = builder.Build();
+var connectionString = config["ConnectionString"];
+
+string appName = config["ApplicationSettings:AppName"];
+
+Debug.WriteLine(connectionString);
+Debug.WriteLine(appName);
+
+
+// 绑定配置到对象
+AppSettings appSettings = new AppSettings
+{
+    Logging = new LoggingSettings(),
+    Application = new ApplicationSettings()
+};
+config.GetSection("Logging").Bind(appSettings.Logging);
+config.GetSection("ApplicationSettings").Bind(appSettings.Application);
+
+Debug.WriteLine($"Log Level: {appSettings.Logging.LogLevel.Default}");
+Debug.WriteLine($"App Name: {appSettings.Application.AppName}");
+Debug.WriteLine($"App Version: {appSettings.Application.Version}");
+
+Console.ReadLine();
