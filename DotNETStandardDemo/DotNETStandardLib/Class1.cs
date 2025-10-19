d55411b68277ffee0f37970163f9581a0d548356
anyu <glepooek@outlook.com>
2025-10-19 21:11:32 +0800

    .net standard demo

---
+++ DotNETStandardDemo/DotNETStandardLib/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+// https://devblogs.microsoft.com/dotnet/the-future-of-net-standard/
+// .NET Standard 2.1是最后一个版本
+// .NET Standard 1.2是支持.NET452的最后一个版本
+// .NET Standard 2.1仅支持.NET3.0及以上，不支持.NET Framework
+
+// .NET Standard不包含WPF API: https://github.com/dotnet/wpf/discussions/9651
+
+namespace DotNETStandardLib
+{
+    public class Class1
+    {
+        TextBlock
+    }
+}
