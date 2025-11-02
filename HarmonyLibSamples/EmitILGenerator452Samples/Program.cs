d0558a073ce2f62a6983dd09898539beac77b51e
anyu <glepooek@outlook.com>
2025-11-02 12:01:55 +0800

    IL指令辅助方法

---
+++ HarmonyLibSamples/EmitILGenerator452Samples/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmitILGenerator452Samples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AssemblyName assemblyName = new AssemblyName("MyDynamicAssembly");
+            AssemblyBuilder assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(
+                assemblyName, 
+                AssemblyBuilderAccess.RunAndSave);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule(
+                    "Mudule1", 
+                    "Module1.netmocule");
+
+            TypeBuilder classBuilder = moduleBuilder.DefineType(
+                    "MyNamespace.MyClass",
+                    TypeAttributes.Public | TypeAttributes.Class);
+
+            ILGeneratorWriteLineDemo.Run(classBuilder);
+            ILGeneratorCatchExceptionDemo.Run(classBuilder);
+            ILGeneratorThrowExceptionDemo.Run(classBuilder);
+            ILGeneratorLocalBuilderDemo.Run(classBuilder);
+
+            Type classType = classBuilder.CreateType();
+
+            assemblyBuilder.Save($"{assemblyName.Name}.dll");
+
+            #region 测试执行
+
+            object dynamicClassInstance1 = Activator.CreateInstance(classType);
+            classType.GetMethod("MyMethod1").Invoke(dynamicClassInstance1, new object[] { 42, "Hello from EmitWriteLine!" });
+
+            object dynamicClassInstance2 = Activator.CreateInstance(classType);
+            classType.GetMethod("MyMethod2").Invoke(dynamicClassInstance2, new object[] { 42, "Hello on try..catch..finally!" });
+
+            object dynamicClassInstance4 = Activator.CreateInstance(classType);
+            object result =classType.GetMethod("MyMethod4").Invoke(dynamicClassInstance4, new object[] { 1, 13 });
+            Console.WriteLine($"MyMethod4 Result: {result}");
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HarmonyLibSamples/EmitILGeneratorSamples/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+AssemblyName assemblyName = new AssemblyName("MyDynamicAssembly");
+AssemblyBuilder assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("Mudule1");
+
+TypeBuilder classBuilder = moduleBuilder.DefineType(
+        "MyNamespace.MyClass",
+        TypeAttributes.Public | TypeAttributes.Class);
+MethodBuilder methodBuilder = classBuilder.DefineMethod(
+         "MyMethod",
+          MethodAttributes.Public,
+          typeof(void),
+          new Type[] { typeof(int), typeof(string) });
+
+methodBuilder.DefineParameter(1, ParameterAttributes.None, "id");
+methodBuilder.DefineParameter(2, ParameterAttributes.Optional, "message");
+
+ILGenerator ilGenerator = methodBuilder.GetILGenerator();
+// 定义本地变量，用于存储格式化后的字符串
+LocalBuilder resultLocal = ilGenerator.DeclareLocal(typeof(string));
+ilGenerator.Emit(OpCodes.Ldstr, "ID: {0}, Message: {1}");
+ilGenerator.Emit(OpCodes.Ldarg_1); // 实例方法获取第一个参数，Ldarg_0是this
+ilGenerator.Emit(OpCodes.Box, typeof(int)); // Box the int argument
+ilGenerator.Emit(OpCodes.Ldarg_2);
+ilGenerator.Emit(OpCodes.Call, typeof(string).GetMethod("Format", new Type[] { typeof(string), typeof(object), typeof(object) }));
+//ilGenerator.Emit(OpCodes.Call, typeof(Console).GetMethod("WriteLine", new Type[] { typeof(string) }));
+// 将结果存储到本地变量
+ilGenerator.Emit(OpCodes.Stloc_0, resultLocal);
+ilGenerator.EmitWriteLine(resultLocal);
+ilGenerator.Emit(OpCodes.Ret);
+
+Type classType = classBuilder.CreateType();
+
+object dynamicClassInstance = Activator.CreateInstance(classType);
+classType.GetMethod("MyMethod").Invoke(dynamicClassInstance, new object[] { 42, "Hello from dynamic method!" });
+
+
+Console.ReadLine();
