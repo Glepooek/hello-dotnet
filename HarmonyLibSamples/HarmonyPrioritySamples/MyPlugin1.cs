d930ec62c404b83629cbb17335b82dfe7214712f
anyu <glepooek@outlook.com>
2025-11-19 23:17:57 +0800

    harmony priority

---
+++ HarmonyLibSamples/HarmonyPrioritySamples/MyPlugin1.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using HarmonyPatch.Shared;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace HarmonyPrioritySamples
+{
+    internal class MyPlugin1
+    {
+        public static void RunPlugin1()
+        {
+            var harmony = new Harmony("net.example.plugin1"); // 初始化Harmony，指定插件ID
+            harmony.PatchAll(Assembly.GetExecutingAssembly()); // 为当前程序集的所有补丁注册
+        }
+
+        [HarmonyLib.HarmonyPatch(typeof(Foo))] // 指定要补丁的目标类为Foo
+        [HarmonyLib.HarmonyPatch("GetSecret")]      // 指定要补丁的目标方法为Bar
+        class MyPatch
+        {
+            //[HarmonyAfter("net.example.plugin2")]
+            [HarmonyPriority(Priority.Low)]
+            // 定义后缀补丁：修改Bar方法的返回结果
+            static void Postfix(ref string __result) => __result = "new secret 1";
+        }
+    }
+}
---
+++ HarmonyLibSamples/HarmonyPrioritySamples/Program.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using HarmonyPatch.Shared;
+using HarmonyPrioritySamples;
+
+MyPlugin1.RunPlugin1();
+MyPlugin2.RunPlugin2();
+
+var foo = Traverse.Create<Foo>()
+    .Method("MakeFoo")
+    .GetValue<Foo>();
+string result = Traverse.Create(foo).Method("GetSecret").GetValue<string>();
+Console.WriteLine(result);
+
+Console.ReadLine();
