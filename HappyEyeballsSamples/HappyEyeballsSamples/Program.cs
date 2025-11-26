d92c8a3fe8a878b5e293c4544ab961d9df47564c
anyu <glepooek@outlook.com>
2025-11-26 22:22:51 +0800

    .net httpclient happy eyeballs算法实现

--- HappyEyeballsSamples/HappyEyeballsSamples/Program.cs
+++ HappyEyeballsSamples/HappyEyeballsSamples/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,21 +13,28 @@ namespace HappyEyeballsSamples
     {
         static void Main(string[] args)
         {
-            CheckDnsResolution("k12-teaching.unipus.cn");
-            CheckDnsResolution("www.baidu.com");
+            //CheckDnsResolution("k12-teaching.unipus.cn");
+            //CheckDnsResolution("www.baidu.com");
+
+            using HttpClient httpClient = HappyEyeballsHttp.CreateHttpClient("http://www.baidu.com/");
+            using HttpResponseMessage response = httpClient.GetAsync("/").Result;
+            string content = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(content);
+
+
             Console.ReadLine();
         }
 
         // 快速检查域名解析结果
         public static async Task CheckDnsResolution(string hostname)
         {
-            var addresses = await Dns.GetHostAddressesAsync(hostname);
-            foreach (var addr in addresses)
+            IPHostEntry entry = await Dns.GetHostEntryAsync(hostname);
+            foreach (var addr in entry.AddressList)
             {
                 Console.WriteLine($"{hostname} -> {addr} ({addr.AddressFamily})");
             }
 
-            if (addresses.Any(ip => ip.AddressFamily == AddressFamily.InterNetworkV6))
+            if (entry.AddressList.Any(ip => ip.AddressFamily == AddressFamily.InterNetworkV6))
             {
                 Console.WriteLine("该域名支持IPv6，HttpClient可能优先使用IPv6");
             }
