3829b83170bb752122bfde3f047fcc106f8aaf7c
anyu <glepooek@outlook.com>
2026-06-28 10:01:04 +0800

    refactor: rename Learning.JsonNet.Shared -> Demo00.Shared, update namespace
    - git mv Learning.JsonNet.Shared/ -> Demo00.Shared/
    - Renamed csproj to Demo00.Shared.csproj
    - Updated all three source files to namespace Demo00.Shared
    - Added nullable-safe initializers (= "") and Address? nullable annotation

---
+++ Learning.JsonNet/Demo00.Shared/Gender.cs
@@ -0,0 +1,8 @@
+namespace Demo00.Shared
+{
+    public enum Gender
+    {
+        Male = 0,
+        Female
+    }
+}
---
+++ Learning.JsonNet/Demo00.Shared/Person.cs
@@ -0,0 +1,10 @@
+namespace Demo00.Shared
+{
+    public class Person
+    {
+        public string Name { get; set; } = "";
+        public int Age { get; set; }
+        public Gender Sex { get; set; }
+        public Address? Address { get; set; }
+    }
+}
